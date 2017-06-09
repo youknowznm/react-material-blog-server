4c2e94befa12cf0df2a3f751b84167470c0bdebb
EnmingZhang <zhangenming@gomeplus.com>
2017-06-09 15:36:39 +0800

    char counter

--- src/_javascripts/common/md-chip.js
+++ src/_javascripts/common/md-chip.js
@@ -3,72 +3,93 @@ import $ from './jquery';
 
 export default function initMdChip() {
 
-    function validate(inputEle) {
-        let $this = $(inputEle);
-        let $wrap = $this.parent();
-        let valRegExpStr = $wrap.data('validatorRegExp');
-        if (valRegExpStr !== undefined) {
-            let valRegExp = new RegExp(valRegExpStr);
-            if (valRegExp.test($this.val())) {
-                $wrap.removeClass('invalid');
-            } else {
-                $wrap.addClass('invalid');
-            }
-        }
-    }
-
     function initChip($mdChip) {
         let chipsData = $mdChip.data('md-chips');
-        if (chipsData !== undefined) {
+        if ($mdChip.find('.chip').length > 0) {
             $mdChip.addClass('non-empty');
-            let arr = chipsData.split(/\s+/);
-            let $chipWrap = $mdChip.children('.chip-wrap');
-            arr.forEach(function(chipStr) {
-                $chipWrap.prepend(
-                    $(`<span class="chip">${chipStr}<i class="btn-remove"></i></span>`)
-                );
-            });
-        } else {
-            $mdChip.removeClass('non-empty');
         }
+        let $input = $mdChip.find('._input');
+        let currentCharCount = 0;
+        let maxCharCount = $input.attr('maxlength');
+        $mdChip.find('.current').text(currentCharCount);
+        $mdChip.find('.maximum').text(maxCharCount);
     }
 
     $('.md-chip').each(function() {
         initChip($(this));
     });
 
-    // $('.md-chip ._input')
-    //     .on('focus', function() {
-    //         let $this = $(this);
-    //         let $wrap = $this.parent();
-    //         if ($this.val() === '') {
-    //             $this.parent().addClass('focused');
-    //         }
-    //     })
-    //     .on('blur', function() {
-    //         let $this = $(this);
-    //         if ($this.val() === '') {
-    //             $this.parent().removeClass('focused');
-    //         }
-    //         validate(this);
-    //     })
-    //     .on('keyup', function(evt) {
-    //         let $this = $(this);
-    //         let $wrap = $this.parent();
-    //         let chipCount = 0;
-    //         if (evt.keyCode === 13) {
-    //             let val = $this.val();
-    //             if (/\S/.test(val) && chipCount < 4) {
-    //                 $wrap.append(
-    //                     $(`<span class="chip">${val}<i class="remove"></i></span>`)
-    //                 );
-    //                 chipCount++;
-    //             }
-    //         }
-    //     })
+    $('.md-chip ._input')
+        .on('focus', function() {
+            let $this = $(this);
+            let $mdChip = $this.parents('.md-chip');
+            if ($this.val() === '') {
+                $mdChip.addClass('focused');
+            }
+        })
+        .on('blur', function() {
+            let $this = $(this);
+            let $mdChip = $this.parents('.md-chip');
+            if ($this.val() === '') {
+                $mdChip.removeClass('focused');
+            }
+            if ($mdChip.find('.chip').length > 0) {
+                $mdChip.addClass('non-empty');
+            }
+        })
+        .on('keyup', function(evt) {
+            let $this = $(this);
+            let $mdChip = $this.parents('.md-chip');
+            let $error = $mdChip.children('.error');
+            let chips = $mdChip.find('.chip');
+            let chipCount = chips.length;
+            if (evt.keyCode === 13) {
+                let val = $this.val().trim();
+                $this.val('');
+                if (/\S/.test(val)) {
+                    // 最大标签数量限制
+                    if (chipCount === 3) {
+                        showError($error, 'Maximum tags reached.');
+                        return;
+                    }
+                    // 同名标签限制
+                    for (let c of chips) {
+                        if (c.innerText === val) {
+                            showError($error, 'Tag already exists.');
+                            return;
+                        }
+                    }
+                    $this.before(
+                        $(`<span class="chip">${val}<i class="btn-remove"></i></span>`)
+                    );
+                }
+            }
+            // 字数验证
+            let currentCount = $this.val().length;
+            let currentCharCounter = $mdChip.find('.current');
+            let maxCharCount = +$mdChip.find('.maximum').text();
+            currentCharCounter.text(currentCount);
+        });
 
-    function getChip(chipContent) {
+    $('.md-chip').on('click', function() {
+        let $tar = $(this).find('._input');
+        if (!$tar.is(':focus')) {
+            $tar.focus();
+        }
+    });
 
+    $('.md-chip').on('click', '.btn-remove', function(evt) {
+        let $tar = $(evt.target).parent('.chip');
+        $tar.fadeOut(function() {
+            $tar.remove();
+        });
+    });
+
+    function showError($ele, str) {
+        $ele.text(str).addClass('show');
+        setTimeout(function() {
+            $ele.removeClass('show');
+        }, 3000);
     }
 
 }
--- src/_javascripts/common/md-input.js
+++ src/_javascripts/common/md-input.js
@@ -4,12 +4,17 @@ import $ from './jquery';
 export default function initMdInput() {
 
     function initInput($mdInput) {
-        let val = $mdInput.children('input').attr('value');
+        let $input = $mdInput.children('input');
+        let val = $input.attr('value');
         if (val !== undefined) {
             $mdInput.addClass('non-empty');
         } else {
             $mdInput.removeClass('non-empty');
         }
+        let currentCharCount = val ? val.length : 0;
+        let maxCharCount = $input.attr('maxlength');
+        $mdInput.find('.current').text(currentCharCount);
+        $mdInput.find('.maximum').text(maxCharCount);
     }
 
     $('.md-input').each(function() {
@@ -21,21 +26,13 @@ export default function initMdInput() {
             let $this = $(this);
             let $wrap = $this.parent();
             $wrap.addClass('focused');
-            if ($this.val() === '') {
-                $wrap.removeClass('non-empty');
-            } else {
-                $wrap.addClass('non-empty');
-            }
+            $wrap.toggleClass('non-empty', $this.val() !== '');
         })
         .on('blur', function() {
             let $this = $(this);
             let $wrap = $this.parent();
             $wrap.removeClass('focused');
-            if ($this.val() === '') {
-                $wrap.removeClass('non-empty');
-            } else {
-                $wrap.addClass('non-empty');
-            }
+            $wrap.toggleClass('non-empty', $this.val() !== '');
             validate(this);
         })
         .on('keyup', function() {
@@ -45,15 +42,18 @@ export default function initMdInput() {
     function validate(inputEle) {
         let $this = $(inputEle);
         let $wrap = $this.parent();
-        let valRegExpStr = $wrap.data('validatorRegExp');
+        // 正则验证
+        let valRegExpStr = $wrap.data('validator');
         if (valRegExpStr !== undefined) {
             let valRegExp = new RegExp(valRegExpStr);
-            if (valRegExp.test($this.val())) {
-                $wrap.removeClass('invalid');
-            } else {
-                $wrap.addClass('invalid');
-            }
+            $wrap.toggleClass('invalid', !valRegExp.test($this.val()));
         }
+        // 字数验证
+        let currentCount = $this.val().length;
+        let currentCharCounter = $wrap.find('.current');
+        let maxCharCount = +$wrap.find('.maximum').text();
+        currentCharCounter.text(currentCount);
+        // $wrap.toggleClass('invalid', currentCount === maxCharCount);
     }
 
 }
