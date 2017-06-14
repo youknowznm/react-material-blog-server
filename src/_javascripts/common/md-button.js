da6ed8f66095e4d4b5db3fdd0c19e0e45b884433
EnmingZhang <zhangenming@gomeplus.com>
2017-06-14 18:36:25 +0800

    md-button done

--- src/_javascripts/common/md-button.js
+++ src/_javascripts/common/md-button.js
@@ -2,42 +2,40 @@ import $ from './jquery';
 
 export default function initMdButton() {
 
-    // 给予每个按钮下的波纹一定的初始宽度
-    function initMdButton($mdButton) {
-        let sideLength = $mdButton.innerWidth();
-        $mdButton.find('.ripple').css({
-            width: sideLength,
-            height: sideLength,
-        });
-    }
-
-    $('.md-button').each(function() {
-        initMdButton($(this));
-    });
-
     $('body')
         .on('mousedown', '.md-button', function(evt) {
             let $this = $(this);
-
-            let $ripple = $this.find('.ripple');
-            let x = evt.offsetX;
-            let y = evt.offsetY;
-            let width = $this.innerWidth();
-            $ripple.css({
-                left: x - width,
-                top: y - width,
-                width: width * 1.5,
-                height: width * 1.5,
-            });
-            $this.addClass('mousedown');
+            if ($this.attr('data-animating') === 'false') {
+                $this.attr('data-clicked', 'true');
+                let $ripple = $this.find('.ripple');
+                let _x = evt.offsetX;
+                let _y = evt.offsetY;
+                let _width = $this.innerWidth();
+                let _height = $this.innerHeight();
+                // 根据事件坐标和按钮水平中点的距离，获取波纹的直径
+                let offsetToHorizontalCenter = _width / 2 - _x;
+                let offsetToVerticalCenter = _height / 2 - _y;
+                let sideLength = _width + Math.abs(offsetToHorizontalCenter) * 2 + (_width / 10);
+                $ripple.css({
+                    width: sideLength,
+                    height: sideLength,
+                    left: (_width - sideLength) / 2 - offsetToHorizontalCenter,
+                    top: (_height - sideLength) / 2 - offsetToVerticalCenter,
+                });
+                $this.addClass('mousedown');
+            }
         })
-        .on('mouseup', '.md-button', function() {
-            // let $this = $(this);
-            // let $ripple = $this.find('.ripple');
-            // $this.removeClass('mousedown').addClass('mouseup');
-            // setTimeout(function() {
-            //     $this.removeClass('mouseup');
-            // },500)
+        .on('mouseup mouseout', '.md-button', function() {
+            let $this = $(this);
+            if ($this.attr('data-animating') === 'false' && $this.attr('data-clicked') === 'true') {
+                $this.attr('data-animating', 'true');
+                $this.attr('data-clicked', 'false');
+                $this.removeClass('mousedown').addClass('mouseup');
+                setTimeout(function() {
+                    $this.removeClass('mouseup');
+                    $this.attr('data-animating', 'false');
+                }, 250);
+            }
         })
 
 
