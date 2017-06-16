1effe6b860398c003bbb956d7ed62ca51afebfd4
EnmingZhang <znm92@icloud.com>
2017-06-16 16:34:27 +0800

    color pallete

--- src/_javascripts/common/md-button.js
+++ src/_javascripts/common/md-button.js
@@ -3,7 +3,7 @@ import $ from './jquery';
 export default function initMdButton() {
 
     $('body')
-        .on('mousedown', '.md-button-flat:not(._disabled)', function(evt) {
+        .on('mousedown', '.md-button:not(._disabled)', function(evt) {
             let $this = $(this);
             if ($this.data('animating') === false) {
                 $this.data('clicked', true);
@@ -25,7 +25,7 @@ export default function initMdButton() {
                 $this.addClass('mousedown');
             }
         })
-        .on('mouseup mouseout', '.md-button-flat:not(._disabled)', function() {
+        .on('mouseup mouseout', '.md-button:not(._disabled)', function() {
             let $this = $(this);
             if ($this.data('animating') === false && $this.data('clicked') === true) {
 
@@ -48,11 +48,27 @@ export default function initMdButton() {
         })
 
     $('body')
-        .on('mousedown', '.md-button-round', function(evt) {
-            $(this).addClass('mousedown');
-        })
-        .on('mouseup mouseout', '.md-button-round', function() {
-            $(this).removeClass('mousedown');
+        .on('mousedown', '.md-icon-button:not(._disabled)', function(evt) {
+            let $this = $(this);
+            if ($this.data('animating') === false) {
+                $this.data('clicked', true);
+                let $ripple = $this.find('.ripple');
+                let _x = evt.offsetX;
+                let _y = evt.offsetY;
+                let _width = $this.innerWidth();
+                let _height = $this.innerHeight();
+                // 根据事件坐标和按钮水平中点的距离，获取波纹的直径
+                let offsetToHorizontalCenter = _width / 2 - _x;
+                let offsetToVerticalCenter = _height / 2 - _y;
+                let sideLength = _width + Math.abs(offsetToHorizontalCenter) * 2 + (_width / 10);
+                $ripple.css({
+                    width: sideLength,
+                    height: sideLength,
+                    left: (_width - sideLength) / 2 - offsetToHorizontalCenter,
+                    top: (_height - sideLength) / 2 - offsetToVerticalCenter,
+                });
+                $this.addClass('mousedown');
+            }
         })
 
 }
