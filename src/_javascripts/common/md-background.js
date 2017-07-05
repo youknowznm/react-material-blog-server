b407f284cab378a4688fbed0a7eade228fa26fe4
EnmingZhang <znm92@icloud.com>
2017-07-05 17:43:54 +0800

    upd

--- src/_javascripts/common/md-background.js
+++ src/_javascripts/common/md-background.js
@@ -1,8 +1,20 @@
 import $ from "./jquery"
 
-let generateMaterialBackground = function() {
+let generateMaterialBackground = function($ele) {
 
-    
+    let $mdBgWrap = $('<div class="md-bg-wrap"></div>')
+
+    let currentZIndex = 1
+    let mdBlockCount = Math.floor(Math.random() * 8 + 6)
+
+    for (let i = 0; i < blockCount; i++) {
+        addMdBlock()
+        currentZIndez++
+    }
+
+    function addMdBlock() {
+        let shadowStrength = (Math.random() < .5) ? 'light' : 'strong'
+    }
 
 }
 
