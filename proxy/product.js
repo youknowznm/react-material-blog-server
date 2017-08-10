5d35e4856e38fc1f797bc73608c5cecd2f02f084
EnmingZhang <znm92@icloud.com>
2017-08-10 19:03:31 +0800

    product model

---
+++ proxy/product.js
@@ -0,0 +1,52 @@
+let ProductModel = require('../models/product')
+
+// 取得所有产品文档
+function getProducts(cb) {
+    ProductModel.find({}, function(e, doc) {
+        if (e) {
+            console.error(e)
+        }
+        return cb(doc);
+    })
+}
+
+// 保存产品文档
+function saveProduct(params, cb) {
+    let _id = params._id,
+        productDoc = new ProductModel({
+            title: params.title,
+            summary: params.summary,
+            content: params.content,
+            tags: params.tags,
+            created: params.created,
+        })
+    ProductModel.findById(_id, function(e, doc) {
+        if (e) {
+            console.error(e)
+        }
+        if (doc === null) {
+            productDoc.save(function(e) {
+                if (e) {
+                    console.error(e)
+                }
+                return cb()
+            })
+        } else {
+            ProductModel.update(
+                { _id },
+                productDoc,
+                function(e) {
+                    if (e) {
+                        console.error(e)
+                    }
+                    return cb()
+                }
+            )
+        }
+    })
+}
+
+module.exports = {
+    getProducts,
+    saveProduct,
+}
---
+++ routes/_productOverview.js
@@ -0,0 +1,34 @@
+let productProxy = require('../proxy/product')
+let auth = require('../controllers/auth')
+
+module.exports = function(router) {
+
+    /*
+    全部产品
+    */
+    router.get('/products', function(req, res, next) {
+        console.log(req.session);
+        productProxy.getProducts(function(docs) {
+            res.render('productOverview', {
+                navType: 0,
+                pageTitle: 'products',
+                static: 'productOverview',
+                authLevel: auth.getAuthLevel(req),
+                // docs: [],
+                docs,
+            })
+        })
+
+        // {
+        // // router.get('/getproducts', function(docs) {
+        //     res.render('products', {
+        //         pageTitle: 'products',
+        //         static: 'products',
+        //         docs
+        //     })
+        // })
+    })
+
+    return router
+
+}
