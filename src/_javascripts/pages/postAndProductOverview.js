94b14540b3fcf86193b3f43929bb2daf4b48890f
EnmingZhang <znm92@icloud.com>
2017-08-14 15:28:38 +0800

    client tag query logic

--- src/_javascripts/pages/postAndProductOverview.js
+++ src/_javascripts/pages/postAndProductOverview.js
@@ -6,13 +6,39 @@ $(function() {
 
     // 生成md背景
     generateMaterialBackground('.post')
+    generateMaterialBackground('.product')
 
-    $('.post-link').click('.tag', function() {
-        $mdDelay(function(){
-            let targetTag = (this).data('originTag')
-            location.pathName = '/posts?tag=' + targetTag
+    $('.post')
+        // 点击博客的大部分区域，跳转至该博客id的路由
+        .on('click', function() {
+            let postId = $(this).attr('data-id')
+            location.assign('/posts/' + postId)
         })
-    })
+        // 点击博客的任一标签，跳转至含该标签的路由
+        .on('click', '.tag', function(evt) {
+            evt.stopPropagation()
+            let targetTag = $(this).attr('data-origin-tag')
+            rhaegoUtil.mdDelay(function(){
+                location.assign('/posts?tag=' + targetTag)
+            })
+        })
+
+    // TODO
+    // $('.post')
+    //     // 点击博客的大部分区域，跳转至该博客id的路由
+    //     .on('click', function() {
+    //         let postId = $(this).attr('data-id')
+    //         location.assign('/posts/' + postId)
+    //     })
+    //     // 点击博客的任一标签，跳转至含该标签的路由
+    //     .on('click', '.tag', function(evt) {
+    //         evt.stopPropagation()
+    //         let targetTag = $(this).attr('data-origin-tag')
+    //         rhaegoUtil.mdDelay(function(){
+    //             location.assign('/posts?tag=' + targetTag)
+    //         })
+    //     })
+
 
 })
 
