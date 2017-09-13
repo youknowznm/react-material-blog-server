ec3e2eaea0dea9bccb14f50bb223311701b3d468
EnmingZhang <znm92@icloud.com>
2017-09-13 14:54:31 +0800

    rte upd

--- src/_javascripts/common/md-rte.js
+++ src/_javascripts/common/md-rte.js
@@ -10,6 +10,17 @@ export default function initMdRte(options) {
     }
     let actionArr = [
         {
+            abbr: 'undo',
+            fullName: 'undo',
+            action: () => null,
+        },
+        {
+            abbr: 'redo',
+            fullName: 'redo',
+            action: () => null,
+            followedBySeparator: true,
+        },
+        {
             abbr: 'b',
             fullName: 'bold',
             action: function() {
@@ -30,6 +41,7 @@ export default function initMdRte(options) {
             abbr: 's',
             fullName: 'strikethrough',
             action: () => format('strikeThrough'),
+            followedBySeparator: true,
         },
         {
             abbr: 'h',
@@ -49,6 +61,11 @@ export default function initMdRte(options) {
             action: () => format('formatBlock', '<blockquote>'),
         },
         {
+            abbr: 'code',
+            fullName: 'code',
+            action: () => format('formatBlock', '<pre>'),
+        },
+        {
             abbr: 'ol',
             fullName: 'ordered list',
             action: () => format('insertOrderedList'),
@@ -57,17 +74,9 @@ export default function initMdRte(options) {
             abbr: 'ul',
             fullName: 'unordered list',
             action: () => format('insertUnrderedList'),
+            followedBySeparator: true,
         },
-        {
-            abbr: 'code',
-            fullName: 'code',
-            action: () => format('formatBlock', '<pre>'),
-        },
-        {
-            abbr: 'hr',
-            fullName: 'horizontal line',
-            action: () => format('insertHorizontalRule'),
-        },
+
         {
             abbr: 'link',
             fullName: 'link',
@@ -79,31 +88,33 @@ export default function initMdRte(options) {
             action: () => null,
         },
         {
-            abbr: 'undo',
-            fullName: 'undo',
-            action: () => null,
+            abbr: 'hr',
+            fullName: 'horizontal line',
+            action: () => format('insertHorizontalRule'),
+            followedBySeparator: true,
         },
         {
-            abbr: 'redo',
-            fullName: 'redo',
+            abbr: 'clear',
+            fullName: 'clear format',
             action: () => null,
         },
     ]
     let rteHTML = '<ul class="actions">'
     actionArr.forEach(function(action) {
         // 已有该行为的对应图标时，使用图标；否则用字符串标识之
-        let contentHTML = action.textContentHTML || '<span class="icon-wrap"><i class="icon"></i></span>'
-        rteHTML += `<li data-action-abbr="${action.abbr}" class="action show-tooltip">
-            ${contentHTML}
-            <p class="md-tooltip to-show-at-top">${action.fullName}</p>
-
-        </li>
-        <div class="separator"></div>`
+        rteHTML += `
+            <li data-action-abbr="${action.abbr}" class="action show-tooltip">
+                ${action.textContentHTML || '<span class="icon-wrap"><i class="icon"></i></span>'}
+                <p class="md-tooltip to-show-at-top">${action.fullName}</p>
+            </li>
+            ${action.followedBySeparator === true ? '<div class="separator"></div>' : ''}
+        `
     })
     rteHTML += '</ul><div class="content" contenteditable="true" spellcheck="false"></div>'
     $mdRte
         .html(rteHTML)
         // IDEA 在可编辑区域获得焦点时，execCommand才起作用。否则返回false；而mousedown事件会夺取焦点
+        // https://stackoverflow.com/questions/12525087/why-doesnt-the-document-execcommand-work-when-i-click-on-a-div
         .on('mousedown', '.action', function(evt) {
             evt.preventDefault()
         })
