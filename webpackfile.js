6c712fbba106da5c8bedc4da082ad8fac1ba50ab
EnmingZhang <znm92@icloud.com>
2017-09-14 17:10:50 +0800

    webpack conf

--- webpackfile.js
+++ webpackfile.js
@@ -34,33 +34,39 @@ module.exports = {
         filename: '[name]',
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015'],
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015'],
+                    },
                 },
             },
-        }, {
-            test: /\.scss$/,
-            use: [{
-                loader: "style-loader" // 将 JS 字符串生成为 style 节点
-            }, {
-                loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
-            }, {
-                loader: "sass-loader" // 将 Sass 编译成 CSS
-            }, ],
-        }, {
-            // IDEA: webpack 字体打包
-            test: /\.(jpg|png|ttf)$/,
-            use: [
-                {
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                    },
+                    {
+                        loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                    },
+                    {
+                        loader: "sass-loader" // 将 Sass 编译成 CSS
+                    },
+                ],
+            },
+            {
+                // IDEA: webpack 字体打包
+                test: /\.(jpg|png|ttf)$/,
+                use: {
                     loader: 'url-loader',
                 },
-            ],
-        }],
+            },
+        ],
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
