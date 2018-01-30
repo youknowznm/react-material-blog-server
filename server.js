a8b58bfead1beda81208371ddce4b5d00dcc4870
youknowznm <znm92@icloud.com>
2018-01-30 16:33:12 +0800

    use native node

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url = require('url')
+var proxyaddr = require('proxy-addr')
 
 const lazy = function* (arr) {
   yield* arr
@@ -16,23 +17,25 @@ const passRouter = (allRoutes, targetMethod, targetPath) => (req, res) => {
       res.end(`Cannot ${method} ${path}`)
       return
     }
+
     const {method, path, fn} = thisRoute
 
     // 匹配到了符合的路由
     // 路由path为*时匹配所有请求的方法
     // 路哟method为all时陪陪所有请求的路径
-    if ((method === targetMethod || method === 'all')
-        && (path === targetPath || path === '*')
-    ) {
+    if ([targetMethod, 'all'].includes(method) && [targetPath, '*'].includes(path)) {
       fn(req, res)
       return
     }
 
     // 匹配到了中间件
     // || targetPath.startsWith(path.concat('/'))
-    if (method === 'use'
-      && (path === '/' || path === targetPath)
-    ) {
+
+    if (method === 'use' && [targetPath, '/'].includes(path)) {
+    // if (method === 'use'
+    //   && (path === '/'
+    //   || path === targetPath
+    //   || targetPath.startsWith(path.concat('/')))) {
       fn(req, res, next)
       return
     }
@@ -62,6 +65,14 @@ methods.forEach((method) => {
   }
 })
 
+app.use = (path, fn) => {
+  app.routes.push({
+    method: 'use',
+    path: path,
+    fn: fn
+  })
+}
+
 app.listen = (port = 4000, host = 'localhost') => {
   http
     .createServer(app)
@@ -70,12 +81,38 @@ app.listen = (port = 4000, host = 'localhost') => {
     })
 }
 
-app.get('/', (req, res) => {
-  res.end('shou ye')
+// //////////////
+
+
+app.use('/g', (req, res, next) => {
+  console.log(url.parse(req.url).query);
+  if (url.parse(req.url).query === 'fuck=shit') {
+    next()
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8;'});
+    res.end('对不起，你没有相应权限');
+  }
 })
+
+app.get('/', (req, res) => {
+  console.log('proxyaddr: ', proxyaddr(req, 'loopback'));
+  res.end(proxyaddr(req, 'loopback'))
+})
+
+
+
 app.get('/g', (req, res) => {
   res.end('gg')
 })
 
+
+// app.use('/blog', (req, res, next) => {
+//   if(req.username) {
+//     next();
+//   } else {
+//     res.writeHead(404, {'Content-Type': 'text/html'});
+//     res.end('对不起，你没有相应权限');
+//   }
+// });
 
 app.listen()
